bacbbcf188b6eaadbe1df9a17cb77f774e034aaf
pasangeek <pasan.madusanka9@gmail.com>
2024-07-04 20:37:09 +0900

    gradle update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,4 +66,8 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    // system UI Controller
+    implementation(libs.accompanist.systemuicontroller)
+    // Extended Icons
+    implementation(libs.androidx.material.icons.extended)
 }
