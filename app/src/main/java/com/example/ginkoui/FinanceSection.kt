1b3465f9820688be164617f32e3bc59fb59fc1ec
pasangeek <pasan.madusanka9@gmail.com>
2024-07-08 20:09:23 +0900

    finance section implementation

---
+++ app/src/main/java/com/example/ginkoui/FinanceSection.kt
@@ -0,0 +1,27 @@
+package com.example.ginkoui
+
+val financeList = listOf(
+    Finance(
+        icon = Icons.Rounded.StarHalf,
+        name = "My\nBusiness",
+        background = OrangeStart
+    ),
+
+    Finance(
+        icon = Icons.Rounded.Wallet,
+        name = "My\nWallet",
+        background = BlueStart
+    ),
+
+    Finance(
+        icon = Icons.Rounded.StarHalf,
+        name = "Finance\nAnalytics",
+        background = PurpleStart
+    ),
+
+    Finance(
+        icon = Icons.Rounded.MonetizationOn,
+        name = "My\nTransactions",
+        background = GreenStart
+    ),
+)
